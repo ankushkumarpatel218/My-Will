6f8d87a3399a2b67d28a1a80a0a5963130ea3a10
Ankush <ankushkumarpatel218@gmail.com>
2022-08-28 12:49:25 +0530

    Making some space

--- Sum of natural numbers.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("enter:-"))
-
-Sum = 0
-Sum1 = []
-while num1!=Sum:
-    Sum+=1
-    Sum1.append(Sum)
-Sum2 = sum(Sum1)
-print(Sum2)
--- multiplication of all elemnts of list.py
+++
@@ -1,6 +0,0 @@
-def multiply(list2):
-    mul = 1
-    for k in range(len(list2)):
-        num2 = list2.pop()
-        mul *= num2
-    return mul
