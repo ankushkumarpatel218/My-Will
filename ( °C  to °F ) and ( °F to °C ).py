dbe49333c06cb3259487307639fac53293edf37a
Ankush <ankushkumarpatel218@gmail.com>
2022-08-31 21:46:02 +0530

    changing some lines

--- ( °C  to °F ) and ( °F to °C ).py
+++ ( °C  to °F ) and ( °F to °C ).py
@@ -1,9 +1,9 @@
-def Fahrenheit(temp):
+def fahrenheit(temp):
     Fahren = (temp * 9 / 5) + 32
     return Fahren
 
 
-def Celsius(temp):
+def celsius(temp):
     celsius = (temp * 5 / 9) - 32
     return celsius
 
@@ -13,11 +13,11 @@ while True:
     ch = input("enter your choice: ")
     if ch == "1":
         cel = float(input("enter temperature in Celsius:- "))
-        print(f"\n{cel}°C = {Fahrenheit(cel)}°F")
+        print(f"\n{cel}°C = {fahrenheit(cel)}°F")
 
     elif ch == "2":
         far = float(input("enter temperature in Fahrenheit:- "))
-        print(f"\n{far}°F = {Celsius(far)}°C")
+        print(f"\n{far}°F = {celsius(far)}°C")
     CH = input("\nDo You want to Quit?( * )- ")
     if CH == '*':
         break
