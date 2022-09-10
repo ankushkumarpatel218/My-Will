1b83ed88b1e68ba51f7a7d495df3b64093b0a9d0
Ankush <ankushkumarpatel218@gmail.com>
2022-09-10 17:59:48 +0530

    practice python programs

---
+++ Check if two lists have atleast one element common.py
@@ -0,0 +1,18 @@
+list1 = [10, 20, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
+list2 = [101, 210, 130, 20, 120, 30]
+list3 = []
+if len(list2)>len(list1):
+    for i in list1:
+        if i in list2:
+            list3.append(i)
+    print(f"common elements:{list3}")
+elif len(list2)<len(list1):
+    for i in list2:
+        if i in list1:
+            list3.append(i)
+    print(f"common elements:{list3}")
+else:
+    for i in list1:
+        if i in list2:
+            list3.append(i)
+    print(f"common elements:{list3}")
---
+++ Random value form list of lists.py
@@ -0,0 +1,7 @@
+import random
+
+list1 = [[4, 5, 5], [2, 7, 4], [8, 6, 3]]
+rand = random.choice(list1)
+rand1 = random.choice(rand)
+
+print(f"random number from random list :{rand1}")
