352746085527c80ec8291cd0ebec054e67554f30
Ankush <ankushkumarpatel218@gmail.com>
2022-09-27 13:38:17 +0530

    practice python programs

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 rand_num = random.randint(1, 100)
 guesses = 0
@@ -15,3 +16,19 @@ while True:
     else:
         print(f"Correct! You guessed it right in {guesses} guesses\n")
         break
+
+if os.path.isfile('score.txt') == False:
+    with open('score.txt', 'w') as Score:
+        Score.write(str(guesses))
+else:
+    pass
+with open('score.txt', 'r') as Score:
+      Read = Score.read()
+      if int(Read) > guesses:
+          print(f"you broke the highest score")
+          with open('score.txt', 'w') as Score:
+              Score.write(str(guesses))
+      else:
+          pass
+
+
